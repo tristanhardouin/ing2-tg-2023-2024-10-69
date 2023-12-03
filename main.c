ed69e0a9479e1e4fea6914dad1f3278b4598c714
tristanhardouin <tristan.hardouin@edu.ece.fr>
2023-12-03 17:56:17 +0100

    debut main

--- main.c
+++ main.c
@@ -1,3 +1,13 @@
 //
 // Created by trist_wbwnhau on 03/12/2023.
 //
+#include <stdio.h>
+
+int main() {
+    // Déclaration des variables
+    FILE *fichier;
+    char nomFichier[] = "temps_cycle.txt";
+    int tempsDeCycle;
+
+
+}

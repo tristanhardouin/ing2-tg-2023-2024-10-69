97bd8d31719e61a6c2ff11892ef1d6b5aacd94af
tristanhardouin <tristan.hardouin@edu.ece.fr>
2023-12-03 18:00:22 +0100

    temps_cycle.txt

--- main.c
+++ main.c
@@ -9,5 +9,13 @@ int main() {
     char nomFichier[] = "temps_cycle.txt";
     int tempsDeCycle;
 
+    // Ouverture du fichier en mode lecture
+    fichier = fopen(nomFichier, "r");
+
+    // Vérification si le fichier est ouvert avec succès
+    if (fichier == NULL) {
+        fprintf(stderr, "Erreur lors de l'ouverture du fichier %s\n", nomFichier);
+        return 1; // Quitter le programme avec un code d'erreur
+    }
 
 }

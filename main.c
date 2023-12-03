35d013227e555062f4f63b368a33635fc65d8f69
tristanhardouin <tristan.hardouin@edu.ece.fr>
2023-12-03 22:49:22 +0100

    temps_cycle.txt

--- main.c
+++ main.c
@@ -1,31 +1,9 @@
-//
-// Created by trist_wbwnhau on 03/12/2023.
-//
-#include <stdio.h>
+struct operation {
+    int duree;
+    struct operation *suivant; // Ajout d'un pointeur vers la prochaine opération
+};
 
-int main() {
-    // Déclaration des variables
-    FILE *fichier;
-    char nomFichier[] = "temps_cycle.txt";
-    int tempsDeCycle;
-
-    // Ouverture du fichier en mode lecture
-    fichier = fopen(nomFichier, "r");
-
-    // Vérification si le fichier est ouvert avec succès
-    if (fichier == NULL) {
-        fprintf(stderr, "Erreur lors de l'ouverture du fichier %s\n", nomFichier);
-        return 1; // Quitter le programme avec un code d'erreur
-    }
-
-    // Lecture du temps de cycle depuis le fichier
-    fscanf(fichier, "%d", &tempsDeCycle);
-
-    // Fermeture du fichier
-    fclose(fichier);
-
-    // Affichage du temps de cycle
-    printf("Le temps de cycle est de %d secondes.\n", tempsDeCycle);
-
-    return 0; // Terminer le programme avec succès
-}
+// Définition de la structure temps_cycle
+struct temps_cycle {
+    int duree;
+};

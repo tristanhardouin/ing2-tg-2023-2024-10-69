5db3ace8757f25285350cdef72d6366c6c56d16e
tristanhardouin <tristan.hardouin@edu.ece.fr>
2023-12-03 22:52:09 +0100

    temps_cycle.txt

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Définition de la structure operation
 struct operation {
     int duree;
     struct operation *suivant; // Ajout d'un pointeur vers la prochaine opération
@@ -7,3 +11,78 @@ struct operation {
 struct temps_cycle {
     int duree;
 };
+
+// Fonction pour lire le temps de cycle
+struct temps_cycle lire_temps_cycle() {
+    // Ouvrir le fichier en lecture
+    FILE *fp = fopen("temps_cycle.txt", "r");
+
+    // Vérifier si le fichier est ouvert avec succès
+    if (fp == NULL) {
+        perror("Erreur lors de l'ouverture du fichier temps_cycle.txt");
+        exit(EXIT_FAILURE);
+    }
+
+    // Lire la première ligne du fichier
+    char ligne[10];
+    fgets(ligne, sizeof(ligne), fp);
+
+    // Fermer le fichier
+    fclose(fp);
+
+    // Convertir la chaîne de caractères en un entier
+    int duree = atoi(ligne);
+    printf("Le temps de cycle est de : %d\n",duree);
+    // Créer la structure temps_cycle
+    struct temps_cycle temps_cycle = {.duree = duree};
+
+    return temps_cycle;
+}
+
+// Fonction pour lire la liste des opérations
+struct operation *lire_operations() {
+    // Ouvrir le fichier en lecture
+    FILE *fp = fopen("operations.txt", "r");
+
+    // Vérifier si le fichier est ouvert avec succès
+    if (fp == NULL) {
+        perror("Erreur lors de l'ouverture du fichier operations.txt");
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialiser la liste des opérations
+    struct operation *operations = NULL;
+
+    // Lire les lignes du fichier
+    while (!feof(fp)) {
+        // Allouer de l'espace pour une nouvelle opération
+        struct operation *nouvelle_operation = malloc(sizeof(struct operation));
+
+        // Vérifier si l'allocation a réussi
+        if (nouvelle_operation == NULL) {
+            perror("Erreur lors de l'allocation mémoire");
+            exit(EXIT_FAILURE);
+        }
+
+        // Lire la durée de l'opération
+        fscanf(fp, "%d", &nouvelle_operation->duree);
+        nouvelle_operation->suivant = NULL; // Initialiser le pointeur suivant
+
+        printf("%d\n",nouvelle_operation->duree);
+        // Ajouter la nouvelle opération à la liste
+        if (operations == NULL) {
+            operations = nouvelle_operation; // Première opération dans la liste
+        } else {
+            struct operation *temp = operations;
+            while (temp->suivant != NULL) {
+                temp = temp->suivant;
+            }
+            temp->suivant = nouvelle_operation; // Ajouter à la fin de la liste
+        }
+    }
+
+    // Fermer le fichier
+    fclose(fp);
+
+    return operations;
+}

45d4e52559d1ebbe17dfff5c0b93c20ee914ffbf
RubensLA <131462128+RubensLA@users.noreply.github.com>
2023-12-05 22:44:20 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,45 @@
+/*int main()
+{
+    t_Graphe * g, *g2;
+    int first,i;
+    char nom_fichier[50];
 
+    printf("entrer le nom du fichier du graphe:");
+    gets(nom_fichier);
+    g = lire_graphe(nom_fichier,0);
+
+
+    /// afficher le graphe
+    graphe_afficher(g);
+
+    Reinitialisation 
+    // ordre +2 pour correction bug CreerArete
+    for(i=0;i<g->ordre;i++)
+    {
+        if(g->pSommet[i]->marquageFauxSommet==0){
+            i++;
+        }
+        g->pSommet[i]->couleur=i;
+    }
+
+    // Recherche composantes connexes
+    printf("\nListe composantes connexes\n");
+
+    // reconstruction du graphe si jamais oriente
+    g2 = lire_graphe(nom_fichier,1);
+
+    Reinitialisation 
+    // ordre +2 pour correction bug CreerArete
+    for(i=0;i<g2->ordre;i++)
+    {
+        g2->pSommet[i]->couleur='B';
+    }
+    recherche_connexe(g2);
+
+    tempsDeCycle(g2);
+
+    return 0;
+}*/
 #include <stdio.h>
 #include <stdlib.h>
 
